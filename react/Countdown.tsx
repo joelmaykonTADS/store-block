ccc539c5ebaa910112878f751717aa75d17245b7
joelmaykon <joelmaykon94@gmail.com>
2020-08-22 21:07:31 -0300

    usando o graphql

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import { TimeSplit } from "./typings/global";
 import { tick } from "./utils/time";
 import { useCssHandles } from "vtex.css-handles";
+import { useQuery } from "react-apollo";
+import useProduct from "vtex.product-context/useProduct";
+import productReleaseDate from "./queries/productReleaseDate.graphql";
+
 
 const DEFAULT_TARGET_DATE = new Date("2020-08-25").toISOString();
 const CSS_HANDLES = ["container", "countdown", "title"];
@@ -9,9 +13,21 @@ interface CountdownProps {
     targetDate: string;
 }
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
-    targetDate = DEFAULT_TARGET_DATE
-}) => {
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = () => {
+   
+    const {
+        product: { linkText }
+    } = useProduct();
+    const { data } = useQuery(productReleaseDate, {
+        variables: {
+            slug: linkText
+        },
+        ssr: false
+    });
+console.log({data})
+    
+
     const [timeRemaining, setTime] = useState<TimeSplit>({
         hours: "00",
         minutes: "00",
@@ -20,14 +36,16 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
 
     const handles = useCssHandles(CSS_HANDLES);
 
-    tick(targetDate, setTime);
-
+    tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime)    
+    
     return (
-        <div className={`${handles.container} t-heading-2 fw3 w-100 pt7 pb6 c-muted-1 db tc`}>
+        <div
+            className={`${handles.container} t-heading-2 fw3 w-100 pt7 pb6 c-muted-1 db tc`}
+        >
             <div className={`${handles.countdown} db tc`}>
                 {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
             </div>
-            </div>
+        </div>
     );
 };
 
